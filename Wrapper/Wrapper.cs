099c02e5ee76743c436f9a97b9343b953feca94e
Luke Spragg <luke@spragg.me>
2015-03-26 15:05:55 -0400

    Add wrapper to detect errors in plugin files, thanks to @nogrod - Wrapper detects errors in plugin files, excludes them, and compiles a batch without all broken plugin files. If one or a few plugins in a large batch has errors, the batch doesn't have to be recompiled separately. - Update monolite compiler (basic.exe) to 123

---
+++ Wrapper/Wrapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using Mono.CSharp;
+
+namespace Oxide.Wrapper
+{
+    class Wrapper
+    {
+        static int Main(string[] args)
+        {
+            var result = 0;
+            var outString = new StringBuilder();
+            var error = new StringWriter();
+            var tries = 0;
+            var regex = new Regex(@"([\w\.]+)\(\d+,\d+\): error|error \w+: Source file `[\\\./]*([\w\.]+)", RegexOptions.Compiled);
+            while (!CompilerCallableEntryPoint.InvokeCompiler(args, error))
+            {
+                var cmd = new CommandLineParser(error);
+                var settings = cmd.ParseArguments(args);
+                if (settings == null || settings.FirstSourceFile == null)
+                {
+                    result = 1;
+                    break;
+                }
+                var matches = regex.Matches(error.GetStringBuilder().ToString());
+                var files = new HashSet<string>();
+                foreach (Match match in matches)
+                {
+                    for (var i = 1; i < match.Groups.Count; i++)
+                    {
+                        if (string.IsNullOrWhiteSpace(match.Groups[i].Value)) continue;
+                        files.Add(match.Groups[i].Value);
+                    }
+                }
+                if (files.Count == 0 || files.Count >= settings.SourceFiles.Count || tries++ > 10)
+                {
+                    result = 1;
+                    break;
+                }
+                args = files.Aggregate(args, (current, file) => current.Where(arg => arg.StartsWith("/") || !arg.EndsWith(file)).ToArray());
+                error.WriteLine("Warning: restarting compilation");
+                outString.Append(error.GetStringBuilder());
+                error.GetStringBuilder().Clear();
+            }
+            Console.Write(outString.Append(error.GetStringBuilder()));
+            Environment.Exit(result);
+            return result;
+        }
+    }
+}
