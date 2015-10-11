650e484a38094ce6ab8bc7328ddae47c2790f24d
Luke Spragg <luke@spragg.me>
2015-10-10 22:13:54 -0400

    Generalize and update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ORG_NAME=${TRAVIS_REPO_SLUG%/*}
+REPO_NAME=${TRAVIS_REPO_SLUG#*/}
+
 function die_with { echo "$*" >&2; exit 1; }
 
 echo "Are you Travis?"
@@ -13,29 +16,33 @@ git config --global user.email "travis@travis-ci.org" || die_with "Failed to con
 git config --global user.name "Travis" || die_with "Failed to configure git user name!"
 
 echo "Cloning Oxide repo using token"
-GIT_REPO="https://$GITHUB_TOKEN@github.com/OxideMod/Oxide.git"
-git clone -q --depth 1 $GIT_REPO $HOME/Oxide >/dev/null || die_with "Failed to clone Oxide repository!"
+GIT_REPO="https://$GITHUB_TOKEN@github.com/$ORG_NAME/Oxide.git"
+git clone --depth 1 $GIT_REPO $TRAVIS_BUILD_DIR/Oxide >/dev/null || die_with "Failed to clone Oxide repository!"
 
-echo "Copying files to Oxide directory"
-cp -f CSharpCompiler $HOME/Oxide/Oxide.Ext.CSharp/Dependencies/Linux/CSharpCompiler
-cp -f libmono-2.0.so.1 $HOME/Oxide/Oxide.Ext.CSharp/Dependencies/Linux/libmono-2.0.so.1
-
-echo "Adding and committing"
-cd $HOME/Oxide || die_with "Failed to change to Oxide directory!"
-git add . || die_with "Failed to add files for commit!"
-COMMIT_MESSAGE="CSharpCompiler Linux build $TRAVIS_BUILD_NUMBER from https://github.com/$TRAVIS_REPO_SLUG/commit/${TRAVIS_COMMIT:0:7}"
-git commit -m "$COMMIT_MESSAGE" || die_with "Failed to commit files!"
-
-git config http.postBuffer 52428800
-git config pack.windowMemory "32m"
-git repack --max-pack-size=100M -a -d
-
-echo "Deploying to GitHub"
+cd $TRAVIS_BUILD_DIR/Oxide || die_with "Failed to change to Oxide directory!"
+​
 ATTEMPT=0
 until [ $ATTEMPT -ge 5 ]; do
-    git pull && git push -q origin master >/dev/null && break
+    echo "Fetching any changes from Oxide repository"
+    git fetch $GIT_REPO || die_with "Failed to fetch changes to Oxide!"
+    git reset --hard origin/master
+
+    echo "Copying file(s) to Oxide directory"
+    cp -vf $TRAVIS_BUILD_DIR/{CSharpCompiler,libmono-2.0.so.1} Oxide.Ext.CSharp/Dependencies/Linux || die_with "Failed to copy changes to Oxide!"
+
+    echo "Adding and committing changes"
+    git add . || die_with "Failed to add files for commit!"
+    COMMIT_MESSAGE="$REPO_NAME Linux build $TRAVIS_BUILD_NUMBER from https://github.com/$TRAVIS_REPO_SLUG/commit/${TRAVIS_COMMIT:0:7}"
+    git commit -m "$COMMIT_MESSAGE" || die_with "Failed to commit files!"
+
+    git config http.postBuffer 52428800
+    git config pack.windowMemory "32m"
+    git repack --max-pack-size=100M -a -d
+
+    echo "Pushing build to GitHub"
+    git push -q origin master >/dev/null && break || die_with "Failed to push to GitHub!"
     ATTEMPT=$[$ATTEMPT+1]
     sleep 15
-done || die_with "Failed to push to GitHub!"
+done || die_with "Failed to deploy build!"
 
 echo "Deployment cycle completed. Happy developing!"
