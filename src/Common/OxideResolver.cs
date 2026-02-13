c2084bcb259181ba5b9bfb22a7eebfc223dca222
scorteanucosmin <cozminsc@gmail.com>
2026-02-13 19:58:16 +0200

    Refactor Oxide Resolver logging

--- src/Common/OxideResolver.cs
+++ src/Common/OxideResolver.cs
@@ -10,8 +10,8 @@ public class OxideResolver : MetadataReferenceResolver
     private readonly ILogger _logger;
     private readonly AppConfiguration _appConfiguration;
     private readonly string _runtimePath;
-
     private readonly HashSet<PortableExecutableReference> _referenceCache;
+    public override bool ResolveMissingAssemblies => true;
 
     public OxideResolver(ILogger<OxideResolver> logger, AppConfiguration appConfiguration)
     {
@@ -23,21 +23,65 @@ public class OxideResolver : MetadataReferenceResolver
 
     public override bool Equals(object? other) => other?.Equals(this) ?? false;
 
-    public override int GetHashCode() => _logger.GetHashCode();
+    public override int GetHashCode() => GetType().GetHashCode();
 
     public override ImmutableArray<PortableExecutableReference> ResolveReference(string reference, string? baseFilePath,
         MetadataReferenceProperties properties)
     {
-        _logger.LogInformation("Resolve: {Reference} {BaseFilePath}", reference, baseFilePath);
+        _logger.LogInformation("Resolving: {Reference} {BaseFilePath}", reference, baseFilePath);
         return ImmutableArray<PortableExecutableReference>.Empty;
     }
 
-    public override bool ResolveMissingAssemblies => true;
+    public override PortableExecutableReference? ResolveMissingAssembly(MetadataReference metadataReference, AssemblyIdentity assemblyIdentity) =>
+        Resolve(metadataReference, assemblyIdentity);
 
-    public override PortableExecutableReference? ResolveMissingAssembly(MetadataReference definition, AssemblyIdentity referenceIdentity) =>
-        Reference(definition.Display!);
+    public PortableExecutableReference? Resolve(MetadataReference metadataReference, AssemblyIdentity assemblyIdentity)
+    {
+        string? name = metadataReference.Display;
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return null;
+        }
 
-    public PortableExecutableReference? Reference(string? name)
+        PortableExecutableReference? reference = _referenceCache.FirstOrDefault(r =>
+            Path.GetFileName(r.Display) == name);
+
+        if (reference != null)
+        {
+            return reference;
+        }
+
+        if (name.Equals("System.Private.CoreLib.dll"))
+        {
+            name = "mscorlib.dll";
+        }
+
+        string path = Path.Combine(_appConfiguration.GetDirectoryConfiguration().Libraries, name);
+        FileInfo fileInfo = new(path);
+
+        _logger.LogDebug("Attempting to resolve {0} [{1}] from {2}", name, assemblyIdentity.Version, fileInfo.FullName);
+
+        if (fileInfo.Exists)
+        {
+            reference = MetadataReference.CreateFromFile(fileInfo.FullName);
+            _referenceCache.Add(reference);
+            return reference;
+        }
+
+        fileInfo = new FileInfo(Path.Combine(_runtimePath, name));
+
+        if (fileInfo.Exists)
+        {
+            reference = MetadataReference.CreateFromFile(fileInfo.FullName);
+            _referenceCache.Add(reference);
+            return reference;
+        }
+
+        _logger.LogError("Unable to find required dependency {0}", name);
+        return null;
+    }
+
+    public PortableExecutableReference? AddReference(string? name)
     {
         if (string.IsNullOrWhiteSpace(name))
         {
@@ -52,25 +96,27 @@ public class OxideResolver : MetadataReferenceResolver
             return reference;
         }
 
-        if (name.Equals("System.Private.CoreLib"))
+        if (name.Equals("System.Private.CoreLib.dll"))
         {
             name = "mscorlib.dll";
         }
 
-        FileInfo fileSystem = new(Path.Combine(_appConfiguration.GetDirectoryConfiguration().Libraries, name));
+        string path = Path.Combine(_appConfiguration.GetDirectoryConfiguration().Libraries, name);
+        FileInfo fileInfo = new(path);
 
-        if (fileSystem.Exists)
+        _logger.LogDebug("Adding reference {0} from {1}", name, fileInfo.FullName);
+
+        if (fileInfo.Exists)
         {
-            reference = MetadataReference.CreateFromFile(fileSystem.FullName);
+            reference = MetadataReference.CreateFromFile(fileInfo.FullName);
             _referenceCache.Add(reference);
             return reference;
         }
 
-        fileSystem = new FileInfo(Path.Combine(_runtimePath, name));
-
-        if (fileSystem.Exists)
+        fileInfo = new FileInfo(Path.Combine(_runtimePath, name));
+        if (fileInfo.Exists)
         {
-            reference = MetadataReference.CreateFromFile(fileSystem.FullName);
+            reference = MetadataReference.CreateFromFile(fileInfo.FullName);
             _referenceCache.Add(reference);
             return reference;
         }
